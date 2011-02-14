4dbb2e2dc9be5f6dd9ce2712830fc388921b0a7e
WoLpH <Rick@Fawo.nl>
2011-02-14 05:49:12 +0100

    extended the admin

--- tagging/admin.py
+++ tagging/admin.py
@@ -1,11 +1,27 @@
 from django.contrib import admin
-from tagging.models import Tag, TaggedItem
+from tagging.models import Tag, TaggedItem, TagType
 from tagging.forms import TagAdminForm
 
 class TagAdmin(admin.ModelAdmin):
     form = TagAdminForm
+    search_fields = ('name', 'slug')
+    list_display = ('name', 'url_link', 'usage_count', 'type')
+    list_filter = ('type',)
+    list_editable = ('type',)
 
-admin.site.register(TaggedItem)
+class TagTypeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'no_index', 'created_at', 'updated_at')
+    list_editable = ('no_index',)
+
+class TaggedItemAdmin(admin.ModelAdmin):
+    raw_id_fields = ('tag', 'user')
+    list_display = ('tag', 'user', 'created_at', 'object', 'find_user',
+        'object_link')
+    
+    list_filter = ('created_at',)
+
+admin.site.register(TaggedItem, TaggedItemAdmin)
+admin.site.register(TagType, TagTypeAdmin)
 admin.site.register(Tag, TagAdmin)
 
 
